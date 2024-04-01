704be5cf73755027c86c77e9a7389ee9ee8004b1
송현규 <songhyeongyu@songhyeongyuui-noteubug.local>
2024-04-01 16:18:25 +0900

    alias 수정 및 완료

--- pa1.c
+++ pa1.c
@@ -47,86 +47,13 @@ int run_command(int nr_tokens, char *tokens[])
 
 	char arr[MAX_COMMAND_LEN] = {'\0'};
 	char *arr_tokens[MAX_COMMAND_LEN];
-	struct alias *alias_token_struct = (struct alias *)malloc(sizeof(struct alias));
-	alias_token_struct->alias_name = (char *)malloc(sizeof(char) * sizeof(MAX_COMMAND_LEN));
-	alias_token_struct->alias_detail = (char *)malloc(sizeof(char) * sizeof(MAX_COMMAND_LEN));
-	int alias_name_nr_token = 0;
-	// int alias_detail_nr_token = 0;
-	char alias_name[MAX_COMMAND_LEN] = {'\0'};
-	char alias_detail[MAX_COMMAND_LEN] = {'\0'};
-	char *alias_name_tokens[MAX_COMMAND_LEN];
-	// char *alias_detail_tokens[MAX_COMMAND_LEN];
-
+	struct alias *alias_token_struct[MAX_COMMAND_LEN];
+	int arr_nr_token = 0;
+	static int alias_nr_token = 0;
+	// char *alias_name_arr[MAX_COMMAND_LEN];
+	// char *alias_detail_arr[MAX_COMMAND_LEN];
 	if (strcmp(tokens[0], "exit") == 0)
 		return 0;
-
-	else if (strcmp(tokens[0], "alias") == 0)
-	{
-		pid_t pid;
-		pid = fork();
-		int stat;
-
-		// printf("%d",alias_nr_token);
-		if (nr_tokens > 1)
-		{
-			if (pid == 0)
-			{
-
-				// printf("%s", tokens[1]);
-				for (int i = 1; i < nr_tokens; i++)
-				{
-					if (i == 1)
-					{
-						strcat(alias_token_struct->alias_name, tokens[i]);
-						printf("%s", tokens[i]);
-						printf("alias: %s", alias_token_struct->alias_name);
-					}
-					else if (i != 1)
-					{
-						strcat(alias_token_struct->alias_detail, tokens[i]);
-					}
-
-					if (i != 1 && i < nr_tokens - 1)
-					{
-						strcat(alias_token_struct->alias_detail, " ");
-					}
-				}
-				strcpy(alias_name, alias_token_struct->alias_name);
-				printf("%s\n", alias_name);
-				strcpy(alias_detail, alias_token_struct->alias_detail);
-				printf("%s\n", alias_detail);
-				strcpy(alias_token_struct->alias_name, alias_token_struct->alias_detail);
-
-				return 0;
-			}
-			else
-			{
-				wait(&stat);
-			}
-		}
-		else if (nr_tokens == 1)
-		{
-			if (pid == 0)
-			{
-				printf("alias: %s", alias_token_struct->alias_name);
-				printf("%s", alias_name);
-				printf("%s", alias_name);
-				alias_name_nr_token = parse_command(alias_name, alias_name_tokens);
-				for (int i = 0; i < alias_name_nr_token; i++)
-				{
-					printf("%s", alias_name_tokens[i]);
-				}
-
-				return 0;
-			}
-			else
-			{
-				wait(&stat);
-			}
-		}
-
-		return 1;
-	}
 
 	else if (strcmp(tokens[0], "cd") == 0)
 	{
@@ -144,6 +71,39 @@ int run_command(int nr_tokens, char *tokens[])
 		else
 		{
 			chdir(tokens[1]);
+		}
+
+		return 1;
+	}
+
+	else if (strcmp(tokens[0], "alias") == 0)
+	{
+		if (nr_tokens == 1)
+		{
+			for (int i = 0; i < alias_nr_token; i++)
+			{
+				fprintf(stderr, "%s: %s\n", alias_token_struct[i]->alias_name, alias_token_struct[i]->alias_detail);
+			}
+			return 1;
+		}
+
+		else
+		{
+			alias_token_struct[alias_nr_token] = malloc(sizeof(struct alias));
+			alias_token_struct[alias_nr_token]->alias_name = malloc(sizeof(char) * MAX_COMMAND_LEN);
+			alias_token_struct[alias_nr_token]->alias_detail = malloc(sizeof(char) * MAX_COMMAND_LEN);
+			strcat(alias_token_struct[alias_nr_token]->alias_name, tokens[1]);
+			for (int i = 2; i < nr_tokens; i++)
+			{
+				strcat(alias_token_struct[alias_nr_token]->alias_detail, tokens[i]);
+				if (i < nr_tokens - 1)
+				{
+					strcat(alias_token_struct[alias_nr_token]->alias_detail, " ");
+				}
+			}
+			alias_nr_token++;
+
+			return 1;
 		}
 
 		return 1;
@@ -182,7 +142,26 @@ int run_command(int nr_tokens, char *tokens[])
 						strcat(arr, " ");
 					}
 				}
-				parse_command(arr, arr_tokens);
+				// printf("%s\n",arr);
+				arr_nr_token = parse_command(arr, arr_tokens);
+				// for(int i =0;i<arr_nr_token;i++){
+				// 	printf("%s\n",arr_tokens[i]);
+				// }
+				// printf("%d\n",arr_nr_token);
+
+				for (int i = 1; i < arr_nr_token; i++)
+				{
+					for (int j = 0; j < alias_nr_token; j++)
+					{
+						if (strcmp(arr_tokens[i], alias_token_struct[j]->alias_name) == 0)
+						{
+							// printf("n: %s\n",alias_token_struct[j]->alias_name);
+							// printf("d: %s\n",alias_token_struct[j]->alias_detail);
+							arr_tokens[i] = strdup(alias_token_struct[j]->alias_detail);
+						}
+					}
+				}
+
 				execvp(tokens[0], arr_tokens);
 				free_command_tokens(arr_tokens);
 				return -1;
